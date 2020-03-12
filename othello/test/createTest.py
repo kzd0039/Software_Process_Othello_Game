c6e6081d91eae82e9f01ca7c18bce78b3116a259
dingkun <dingkun@192.168.1.102>
2020-03-12 13:49:24 -0500

    Green Light |

--- othello/test/createTest.py
+++ othello/test/createTest.py
@@ -49,7 +49,7 @@ class createTest(unittest.TestCase):
         correct = {'status': self.error2}
         
         self.setOperation(self.nominalOperation)
-        self.setSize(17)
+        self.setSize(5)
         result = create._create(self.inputDictionary)
         
         self.assertEqual(correct, result)
