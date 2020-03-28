268605d6ed0ef4afb3bc72c902536738ce2540ba
dingkun <dingkun@192.168.1.111>
2020-03-28 17:52:58 -0500

    Red Light |

--- othello/status.py
+++ othello/status.py
@@ -1,3 +1,9 @@
+"""
+    Created to determine the status of the game
+    Baselined: Mar 25, 2020
+    Modified: Mar 28, 2020
+    @Author: Kun Ding
+"""
 def _status(parms):
     result = {'status': 'status stub'}
     return result
--- othello/test/statusTest.py
+++ othello/test/statusTest.py
@@ -1,6 +1,7 @@
 """
-    Created to 
+    Created for integration test of status
     Baselined: Mar 25, 2020
+    Modified: Mar 28, 2020
     @Author: Kun Ding
 """
 
@@ -9,10 +10,42 @@ import othello.status as status
 
 class statusTest(unittest.TestCase):
     def setUp(self):
+        self.nominalLight = 1
+        self.nominalDark = 2
+        self.nominalBlank = 0
+        self.nominalOpeation = 'status'
+        self.inputDictionary = {}
         
-        pass
     def tearDown(self):
-        pass
+        self.inputDictionary = {}
+        
+    def setOperation(self, op):
+        self.inputDictionary['op'] = op
+        
+    def setLight(self, light):
+        self.inputDictionary['light'] = light
+        
+    def setDark(self, dark):
+        self.inputDictionary['dark'] = dark
+        
+    def setBlank(self, blank):
+        self.inputDictionary['blank'] = blank
     
+    def setBoard(self, board):
+        self.inputDictionary['size'] = board
+        
+    def setIntegrity(self, integrity):
+        self.inputDictionary['integrity'] = integrity
+        
+    def setExtra(self, extra):
+        self.inputDictionary['extra'] = extra
+    
+    def TDDtest(self):
+        self.setOperation('status')
+        correct = {'status':'ok'}
+        result = status._status(self.inputDictionary) 
+        self.assertEqual(correct, result)
+        
+        
     
     
