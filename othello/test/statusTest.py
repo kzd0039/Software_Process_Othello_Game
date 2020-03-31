599e877e601baff5be310b2c833d0e7996d91d5c
dingkun <dingkun@192.168.1.111>
2020-03-30 20:07:20 -0500

    Yellow Light |

--- othello/test/statusTest.py
+++ othello/test/statusTest.py
@@ -836,4 +836,23 @@ class statusTest(unittest.TestCase):
        
         correct = {'status': self.error7}
         result = status._status(self.inputDictionary)
+        self.assertEqual(correct, result)  
+        
+    def test200_954(self):
+        self.setOperation('status')
+        self.setLight('1')
+        self.setDark('2')
+        self.setBlank('3')
+        board =[3,3,3,3,3,3,
+                3,3,3,3,3,3,
+                3,3,1,2,3,3,
+                3,3,2,1,3,3,
+                3,3,3,3,3,3,
+                3,3,3,3,3,3]
+        self.setBoard(board)
+        integrity = '4d5aeb4a45b57eecf69dcc304664fcf7a6f7c74c86ef9ede14da46ab2d9df242'
+        self.setIntegrity(integrity)
+       
+        correct = {'status': self.error9}
+        result = status._status(self.inputDictionary)
         self.assertEqual(correct, result)      
