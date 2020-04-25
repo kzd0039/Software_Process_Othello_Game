5d5fd4b29024b0a7d9565f57a19a95afddd791d0
dingkun <dingkun@192.168.1.102>
2020-04-25 10:19:53 -0500

    Red Light |

--- othello/test/placeTest.py
+++ othello/test/placeTest.py
@@ -305,12 +305,7 @@ class statusTest(unittest.TestCase):
         self.setLight('1')
         self.setDark('2')
         self.setBlank('3')
-        board='[3,3,3,3,3,3,\
-               3,3,3,3,3,3,\
-               3,3,1,2,3,3,\
-               3,3,2,1,3,3,\
-               3,3,3,3,3,3,\
-               3,3,3,3,3,3]'
+        board='[3,3,3,3,3,3,3,3,3,3,3,3,3,3,1,2,3,3,3,3,2,1,3,3,3,3,3,3,3,3,3,3,3,3,3,3]'
         self.setBoard(board)
 #         integrity = 'f01977c17f801c43eeb13fb9f74a49bd0c761db3cdffe01510f47ddd23ab465a00'
         correct = {'status': self.error11}
@@ -321,12 +316,7 @@ class statusTest(unittest.TestCase):
         self.setLight('1')
         self.setDark('2')
         self.setBlank('3')
-        board='[3,3,3,3,3,3,\
-               3,3,3,3,3,3,\
-               3,3,1,2,3,3,\
-               3,3,2,1,3,3,\
-               3,3,3,3,3,3,\
-               3,3,3,3,3,3]'
+        board='[3,3,3,3,3,3,3,3,3,3,3,3,3,3,1,2,3,3,3,3,2,1,3,3,3,3,3,3,3,3,3,3,3,3,3,3]'
         self.setBoard(board)
         self.setIntegrity(None)
 #         integrity = 'f01977c17f801c43eeb13fb9f74a49bd0c761db3cdffe01510f47ddd23ab465a00'
