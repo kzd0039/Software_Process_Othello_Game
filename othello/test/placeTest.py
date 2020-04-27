0c91bce9ef5e71371a5d18c8302d1266d8014dea
dingkun <dingkun@192.168.1.102>
2020-04-27 03:07:21 -0500

    Yellow Light |

--- othello/test/placeTest.py
+++ othello/test/placeTest.py
@@ -444,14 +444,36 @@ class statusTest(unittest.TestCase):
         result = place._place(self.inputDictionary)
         self.assertEqual(result, correct)
         
+   
+    def test300_914DarkNull(self):
+        self.setOperation('place')
+        self.setLight('1')
+        self.setDark('')
+        self.setBlank('0')
+        self.setLocation('2:3')
+        board = '[0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,2,0,0,0,0,2,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0]'
+        self.setBoard(board)
+        integrity = '6c3ec0129f5e128f48e2541bd6663a52a825c35f99b9a69d9593f2fc44b0bb4b'
+        self.setIntegrity(integrity)      
+        correct = {'status': self.error1}
+        result = place._place(self.inputDictionary)
+        self.assertEqual(result, correct)
+   
+          
+#     def test300_920BlankNonInteger(self):
 #         self.setOperation('place')
+#         self.setLight('1')
 #         self.setDark('-1')
+#         self.setBlank('0')
+#         self.setLocation('2:3')
+#         board = '[0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,-1,0,0,0,0,-1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0]'
+#         self.setBoard(board)
+#         integrity = '6c3ec0129f5e128f48e2541bd6663a52a825c35f99b9a69d9593f2fc44b0bb4b'
+#         self.setIntegrity(integrity)      
 #         correct = {'status': self.error2}
 #         result = place._place(self.inputDictionary)
-#          
-#         self.assertEqual(result, correct)     
-#          
-#     def test300_920BlankNonInteger(self):
+#         self.assertEqual(result, correct)
+#         
 #         self.setOperation('place')
 #         self.setBlank('X')
 #         correct = {'status': self.error1}
