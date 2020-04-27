c9ad5e276e37de8c1832afc494328056d1b43ee4
dingkun <dingkun@192.168.1.102>
2020-04-27 03:01:33 -0500

    Yellow Light |

--- othello/test/placeTest.py
+++ othello/test/placeTest.py
@@ -399,7 +399,19 @@ class statusTest(unittest.TestCase):
         self.assertEqual(result, correct)
 
          
-#     def test300_911DarkNonInteger(self):
+    def test300_911DarkNonInteger(self):
+        self.setOperation('place')
+        self.setLight('1')
+        self.setDark('1.5')
+        self.setBlank('0')
+        self.setLocation('2:3')
+        board = '[0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1.5,0,0,0,0,1.5,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0]'
+        self.setBoard(board)
+        integrity = '6c3ec0129f5e128f48e2541bd6663a52a825c35f99b9a69d9593f2fc44b0bb4b'
+        self.setIntegrity(integrity)      
+        correct = {'status': self.error1}
+        result = place._place(self.inputDictionary)
+        self.assertEqual(result, correct)
 #         self.setOperation('place')
 #         self.setDark('1.5')
 #         correct = {'status': self.error1}
